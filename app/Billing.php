7dd10444c90e19cd822793757abbfff9d4d6721a
hypertonyc.local <hypertonyc@mail.ru>
2017-07-20 11:18:14 +0300

    billings page

--- app/Billing.php
+++ app/Billing.php
@@ -4,10 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Carbon\Carbon;
+
 class Billing extends Model
 {
   public function billingclients()
   {
     return $this->hasMany('App\Billingclient');
+  }
+
+  public function getCreatedAtAttribute($date)
+  {
+    if(isset($date)) {
+      return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('d-m-Y');
+    } else {
+      return 'Нет данных';
+    }
   }
 }
